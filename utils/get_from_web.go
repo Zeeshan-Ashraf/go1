9a013887543ae25bc6bba8cc07c8e90f422e75c7
Zeeshan <md.ashraf@swiggy.in>
2022-02-26 15:46:20 +0530

    add comment for defer

--- utils/get_from_web.go
+++ utils/get_from_web.go
@@ -9,7 +9,7 @@ import (
 func GetSimpleMsgFromWeb() string {
 	url_wttr := "https://wttr.in/Kolkata"
 	resp, err := http.Get(url_wttr)
-	defer resp.Body.Close()
+	defer resp.Body.Close() //will be executed in last when func will be exiting (if multiple defer then first defer will be executed in last)
 	if err != nil {
 		fmt.Sprintf("error in http.Get err: %s\n", err.Error())
 		fmt.Sprintf("response code: %d\n", resp.StatusCode)
