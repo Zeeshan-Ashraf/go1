d600be4821871b45be1a45d73380330d8474be0a
Zeeshan <md.ashraf@swiggy.in>
2022-02-27 22:43:05 +0530

    how to do post request to web with json data func SendPostReqWithData

--- controllers/basic_server.go
+++ controllers/basic_server.go
@@ -8,6 +8,9 @@ import (
 	"strings"
 )
 
+/*
+simplest get request, returns string (not json) = "Hello There" to users
+*/
 func Hello(c *gin.Context) { //first letter upper case means method is public, c contains request details like url host body cookies headers etc, c is Context which can also validates and serializes JSON
 	c.String(200, "Hello There")
 }
