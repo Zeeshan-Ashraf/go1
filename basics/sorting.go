02b92bc8e752d1d58639195c08bc5a320f352b21
Zeeshan <md.ashraf@swiggy.in>
2022-06-23 14:16:29 +0530

    sort slice

--- basics/sorting.go
+++ basics/sorting.go
@@ -6,7 +6,7 @@ import (
 )
 
 func SortingArray() {
-	//we cant sort Array[struct{}] but slice from go1.18
+	//we cant sort Array[struct{}] but slice from go1.18 how to convert array to slice: slic := arr[:]
 	s := []string{"c", "a", "b"}
 	sort.Strings(s)
 	fmt.Println("Strings:", s)
@@ -21,3 +21,10 @@ func SortingArray() {
 
 //sort slice of struct{} using custom sort func
 //https://stackoverflow.com/questions/28999735/what-is-the-shortest-way-to-simply-sort-an-array-of-structs-by-arbitrary-field
+
+func SortSlice() {
+	a := []int{5, 3, 4, 7, 8, 9}
+	sort.Slice(a, func(i, j int) bool {
+		return a[i] < a[j]
+	})
+}
